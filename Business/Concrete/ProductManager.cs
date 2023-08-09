3cb1c654c8b26c47ab9c39e449a70d37ac665392
Alperen Peynir <alperenpeynir676@gmail.com>
2023-08-09 12:09:15 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstcract;
+using DataAccess.Abstcract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş Kodları
+            //Yetkisi var mı ?
+
+            return _productDal.GetAll();
+
+        }
+    }
+}
